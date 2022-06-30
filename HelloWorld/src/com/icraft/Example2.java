93eccc696988513ee08739037d2b43701851cacf
natnaelareda <natichala2014@gmail.com>
2022-06-30 11:33:57 -0400

    Example2 class commit from eclipse

---
+++ HelloWorld/src/com/icraft/Example2.java
@@ -0,0 +1,13 @@
+package com.icraft;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		  System.out.println("Hello World");
+		  System.out.println("Hello World");
+	}
+
+}
